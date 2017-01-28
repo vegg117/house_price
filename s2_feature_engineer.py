73c37270313bd3ff861dc9e6e756d9cc4d69e98b
vegg117 <vegg117@163.com>
2017-01-29 00:17:07 +0800

    s2

--- s2_feature_engineer.py
+++ s2_feature_engineer.py
@@ -14,15 +14,23 @@ data = pd.read_csv(clean_data_file)
 
 
 # 类别特征，onehot编码
-categorial_features = ['Fence']
+categorial_features = ['BsmtExposure']
 
-data = pd.get_dummies(data=data, columns=categorial_features)
+print 'before categorial:', data.shape
+categorial_data = pd.get_dummies(data[categorial_features])
+# print categorial_data.head()
+# print data[categorial_features].head()
+data = pd.concat([data, categorial_data], axis=1)
+data.drop(categorial_features, axis=1, inplace=True)
+# print data[categorial_features].head()
+print 'after categorial:', data.shape
 
 # 数值特征， 归一化
-numeric_features = ['1stFlrSF']
+numeric_features = ['1stFlrSF', '2ndFlrSF']
 scaler = preprocessing.StandardScaler()
 data[numeric_features] = scaler.fit_transform(data[numeric_features])
 print data.head()
+
 
 
 data.to_csv(feature_data_file, index=False)
